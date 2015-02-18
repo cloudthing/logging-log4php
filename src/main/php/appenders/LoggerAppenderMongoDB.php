b43f81070f503f1965349cddf60f6d911d230f39
Ammar Adil <ammaradil>
2015-02-18 18:20:23 +0530

    Added updates for depreciated functions
    Update "LoggerAppenderMongoDB" to work with new mongo client connect
    functions for mongo version 1.3.0 and above.

--- src/main/php/appenders/LoggerAppenderMongoDB.php
+++ src/main/php/appenders/LoggerAppenderMongoDB.php
@@ -47,7 +47,7 @@ class LoggerAppenderMongoDB extends LoggerAppender {
 	// ******************************************
 	
 	/** Default prefix for the {@link $host}. */	
-	const DEFAULT_MONGO_URL_PREFIX = 'mongodb://';
+	const DEFAULT_MONGO_URL_PREFIX = '';
 	
 	/** Default value for {@link $host}, without a prefix. */
 	const DEFAULT_MONGO_HOST = 'localhost';
@@ -122,15 +122,23 @@ class LoggerAppenderMongoDB extends LoggerAppender {
 	 */
 	public function activateOptions() {
 		try {
-			$this->connection = new Mongo(sprintf('%s:%d', $this->host, $this->port), array('timeout' => $this->timeout));
-			$db	= $this->connection->selectDB($this->databaseName);
-			if ($this->userName !== null && $this->password !== null) {
-				$authResult = $db->authenticate($this->userName, $this->password);
-				if ($authResult['ok'] == floatval(0)) {
-					throw new Exception($authResult['errmsg'], $authResult['ok']);
+			if(version_compare(phpversion('mongo'), '1.3.0', '<')){
+				$this->connection = new Mongo(sprintf('mongodb://%s:%d', $this->host, $this->port), array('timeout' => $this->timeout));
+				$db	= $this->connection->selectDB($this->databaseName);
+				if ($this->userName !== null && $this->password !== null) {
+					$authResult = $db->authenticate($this->userName, $this->password);
+					if ($authResult['ok'] == floatval(0)) {
+						throw new Exception($authResult['errmsg'], $authResult['ok']);
+					}
 				}
+				$this->collection = $db->selectCollection($this->collectionName);
 			}
-			$this->collection = $db->selectCollection($this->collectionName);
+			else{
+				$mongodbServerPath = "mongodb://" . $this->userName . ":" . $this->password . "@" . $this->host . ":" . $this->port . "/" . $this->databaseName;
+				$this->connection = new MongoClient($mongodbServerPath);
+				$db	= $this->connection->selectDB($this->databaseName);
+				$this->collection = $db->selectCollection($this->collectionName);
+			}
 		} catch (MongoConnectionException $ex) {
 			$this->closed = true;
 			$this->warn(sprintf('Failed to connect to mongo deamon: %s', $ex->getMessage()));
